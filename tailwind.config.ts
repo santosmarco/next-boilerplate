d0c2e18d3b58cf6359a3b26d89e1747642db4d24
santosmarco <eu@marco.rio.br>
2023-09-25 15:29:48 -0300

    Add more tailwind plugins

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,28 @@
-import tailwindForms from "@tailwindcss/forms";
+import tailwindAspectRatioPlugin from "@tailwindcss/aspect-ratio";
+import tailwindContainerQueriesPlugin from "@tailwindcss/container-queries";
+import tailwindFormsPlugin from "@tailwindcss/forms";
+import tailwindTypographyPlugin from "@tailwindcss/typography";
 import type { Config } from "tailwindcss";
+import tailwindCssAnimatePlugin from "tailwindcss-animate";
+
+const VAR_FONT_INTER = "var(--font-inter)";
 
 const tailwindConfig: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-inter)"],
+        sans: [VAR_FONT_INTER],
       },
     },
   },
-  plugins: [tailwindForms],
+  plugins: [
+    tailwindAspectRatioPlugin,
+    tailwindContainerQueriesPlugin,
+    tailwindFormsPlugin,
+    tailwindTypographyPlugin,
+    tailwindCssAnimatePlugin,
+  ],
 };
 
 export default tailwindConfig;
